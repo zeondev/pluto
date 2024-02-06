74cb175ee424f837b741cda46daa20bdc9ffadd7
Kat21 <datkat21.yt@gmail.com>
2024-02-06 08:18:29 -0600

    Improve debug app

--- pkgs/apps/Debug.js
+++ pkgs/apps/Debug.js
@@ -17,6 +17,8 @@ export default {
       title: "Debug",
       content: "Hello",
       pid: Root.PID,
+      width: 480,
+      height: 360,
       onclose: () => {
         Root.Lib.onEnd();
       },
@@ -50,13 +52,26 @@ export default {
       });
     console.log(Root.Lib.icons, Root.Lib.icons.length);
 
+    new Root.Lib.html("h2").text("Icons").appendTo(wrapper);
+
+    const iconsContainer = new Root.Lib.html("div")
+      .class("row", "row-wrap", "gap")
+      .appendTo(wrapper);
+
+    const Notify = await Root.Lib.loadLibrary("Notify");
+    const notifyBox = new Root.Lib.html("div")
+      .class("notify-box")
+      .appendTo(wrapper);
+
     for (let key in Root.Lib.icons) {
-      console.log(Root.Lib.icons[key]);
-      new Root.Lib.html("div")
-        .html(Root.Lib.icons[key] + "<br><span>Name: </span>" + key)
-        .appendTo(wrapper)
-        .on("click", (e) => {
-          console.log(e);
+      new Root.Lib.html("button")
+        .class("fc", "gap", "row", "transparent", "m-0")
+        .html(Root.Lib.icons[key])
+        .append(new Root.Lib.html("span").text(key))
+        .appendTo(iconsContainer)
+        .on("click", async (e) => {
+          await navigator.clipboard.writeText(Root.Lib.icons[key]);
+          Notify.show("Copied", `SVG data for ${key} was copied.`, notifyBox);
         });
     }
 
